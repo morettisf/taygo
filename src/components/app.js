ee9239745fedf1971a0fc900980220827f269410
morettisf <morettisf@gmail.com>
2017-08-16 14:16:59 -0700

    completed

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SimpleInlineToolbarEditor from './editor'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SimpleInlineToolbarEditor/>
+      </div>
+    );
+  }
+}
